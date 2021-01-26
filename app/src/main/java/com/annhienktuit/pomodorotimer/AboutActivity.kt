6cb71e1354e6415ac871de6ddb8b70dd6cb1443f
annhienktuit <19520207@gm.uit.edu.vn>
2021-01-26 22:20:14 +0700

    fix about

--- app/src/main/java/com/annhienktuit/pomodorotimer/AboutActivity.kt
+++ app/src/main/java/com/annhienktuit/pomodorotimer/AboutActivity.kt
@@ -1,7 +1,11 @@
 package com.annhienktuit.pomodorotimer
 
+import android.graphics.Color
+import android.os.Build
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.Window
+import android.view.WindowManager
 import androidx.appcompat.app.AppCompatDelegate
 
 class AboutActivity : AppCompatActivity() {
@@ -11,5 +15,10 @@ class AboutActivity : AppCompatActivity() {
         AppCompatDelegate.setDefaultNightMode(
             AppCompatDelegate.MODE_NIGHT_NO
         ); //turn off night mode
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.LOLLIPOP) {
+            val window: Window = window
+            window.addFlags(WindowManager.LayoutParams.FLAG_DRAWS_SYSTEM_BAR_BACKGROUNDS)
+            window.setStatusBarColor(Color.parseColor("#FFFFFF"))
+        }
     }
 }
